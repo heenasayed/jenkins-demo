d6790258cfe17cc7bfc6cb5f47802fe587b69c4e
EC2 Default User <ec2-user@ip-172-31-10-252.ap-south-1.compute.internal>
2026-07-27 21:10:34 +0000

    Added Jenkins build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "================================"
+echo " Jenkins Build Started "
+echo "================================"
+
+date
+
+echo "Running application build..."
+
+echo "Build Completed Successfully"
+
+echo "================================"
+
